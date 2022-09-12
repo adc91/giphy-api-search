03b56134a950c841af44b71818c263f32d45e5d9
Angel Camacho <adc.3191@gmail.com>
2022-09-12 07:34:00 -0400

    Fix typo error on API URL.

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getGifs = async (query) => {
     const { VITE_GIPHY_API_KEY } = import.meta.env;
 
-    const url = `https:/api.giphy.com/v1/gifs/search?api_key=${VITE_GIPHY_API_KEY}&limit=10&q=${query}`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${VITE_GIPHY_API_KEY}&limit=10&q=${query}`;
 
     const response = await fetch(url);
     const { data = [] } = await response.json();
